2def38915b8ab80f58dd902abcb0517a50653c52
Muhammad Sameet <164825370+MuhammadSameet@users.noreply.github.com>
2025-02-23 05:31:33 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -86,7 +86,7 @@ if uploaded_files:
         elif conversion_type == "Excel":
             df.to.to_excel(buffer, index=False)
             file_name = file.name.replace(file_ext, "xlsx")
-            mimi_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         buffer.seek(0)
 
         st.download_button(
